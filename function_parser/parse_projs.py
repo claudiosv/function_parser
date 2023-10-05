f1126d32fdc6f7afc3cbfa6ab0f8a747b83fa166
claudiosv <claudiosv@users.noreply.github.com>
2023-10-05 11:33:37 -0700

    Accept dir path arg

--- function_parser/parse_projs.py
+++ function_parser/parse_projs.py
@@ -8,24 +8,30 @@ from function_parser.process import DataProcessor
 from tree_sitter import Language
 from tqdm import tqdm
 from pathlib import Path
+import argparse
 
-language = "python"
-DataProcessor.PARSER.set_language(
-    Language(
-        os.path.join(function_parser.__path__[0], "tree-sitter-languages.so"), language
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dir_path", type=Path)
+    args = parser.parse_args()
+    language = "python"
+    DataProcessor.PARSER.set_language(
+        Language(
+            os.path.join(function_parser.__path__[0], "tree-sitter-languages.so"), language
+        )
     )
-)
-processor = DataProcessor(
-    language=language, language_parser=LANGUAGE_METADATA[language]["language_parser"]
-)
-
-for i in tqdm(range(1, 51)):
-    dependee = (Path(os.environ['DYPYBENCH_PROJECTS_ROOT']) / f"project{i}").resolve()
-    print(dependee)
-    definitions = processor.process_project(
-        str(dependee), ext=LANGUAGE_METADATA[language]["ext"]
+    processor = DataProcessor(
+        language=language, language_parser=LANGUAGE_METADATA[language]["language_parser"]
     )
-    df = pd.DataFrame(definitions)
-    df.to_parquet(f"dypybench_functions/df_project{i}.parquet")
 
-print("Done!")
+    for i in tqdm(range(1, 51)):
+        dependee = (Path(os.environ['DYPYBENCH_PROJECTS_ROOT']) / f"project{i}").resolve()
+        print(dependee)
+        definitions = processor.process_project(
+            str(dependee), ext=LANGUAGE_METADATA[language]["ext"]
+        )
+        df = pd.DataFrame(definitions)
+        output_path = args.dir_path / f"df_project{i}.parquet"
+        df.to_parquet(str(output_path))
+
+    print("Done!")
