be93a47a1e40cae96dc5c8a2109887dd7ce514ce
claudiosv <claudiosv@users.noreply.github.com>
2023-10-05 13:18:50 -0700

    Revert version

--- function_parser/build_grammars.py
+++ function_parser/build_grammars.py
@@ -8,7 +8,7 @@ _GRAMMARs = {
     # "go": ("https://github.com/tree-sitter/tree-sitter-go.git", "tree-sitter-go", "v0.13.3"),
     # "java": ("https://github.com/tree-sitter/tree-sitter-java.git", "tree-sitter-java", "v0.13.0"),
     # "javascript": ("https://github.com/tree-sitter/tree-sitter-javascript.git", "tree-sitter-javascript", "v0.13.0"),
-    "python": ("https://github.com/tree-sitter/tree-sitter-python.git", "tree-sitter-python", "v0.20.4"),
+    "python": ("https://github.com/tree-sitter/tree-sitter-python.git", "tree-sitter-python", "v0.14.0"),
     # "php": ("https://github.com/tree-sitter/tree-sitter-php.git", "tree-sitter-php", "v0.13.0"),
     # "ruby": ("https://github.com/tree-sitter/tree-sitter-ruby", "tree-sitter-ruby", "v0.13.0")
 }
