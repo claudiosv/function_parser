4a696c63955df547bf0a23b3658cc27f230aab3f
claudiosv <claudiosv@users.noreply.github.com>
2023-10-05 11:43:26 -0700

    Use tree sitter languages

--- function_parser/parse_projs.py
+++ function_parser/parse_projs.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 from function_parser.language_data import LANGUAGE_METADATA
 from function_parser.process import DataProcessor
-from tree_sitter import Language
+from tree_sitter_languages import get_language, get_parser
 from tqdm import tqdm
 from pathlib import Path
 import argparse
@@ -15,17 +15,16 @@ if __name__ == "__main__":
     parser.add_argument("dir_path", type=Path)
     args = parser.parse_args()
     language = "python"
-    DataProcessor.PARSER.set_language(
-        Language(
-            os.path.join(function_parser.__path__[0], "tree-sitter-languages.so"), language
-        )
-    )
+    DataProcessor.PARSER.set_language(get_language(language))
     processor = DataProcessor(
-        language=language, language_parser=LANGUAGE_METADATA[language]["language_parser"]
+        language=language,
+        language_parser=get_parser(language),
     )
 
     for i in tqdm(range(1, 51)):
-        dependee = (Path(os.environ['DYPYBENCH_PROJECTS_ROOT']) / f"project{i}").resolve()
+        dependee = (
+            Path(os.environ["DYPYBENCH_PROJECTS_ROOT"]) / f"project{i}"
+        ).resolve()
         print(dependee)
         definitions = processor.process_project(
             str(dependee), ext=LANGUAGE_METADATA[language]["ext"]
