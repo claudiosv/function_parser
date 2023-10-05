9427c50f09cbb325d10dfd1451c6c92c11db43f2
claudiosv <claudiosv@users.noreply.github.com>
2023-10-05 12:03:31 -0700

    Bump version

--- function_parser/build_grammars.py
+++ function_parser/build_grammars.py
@@ -5,12 +5,12 @@ from pathlib import Path
 from tree_sitter import Language
 
 _GRAMMARs = {
-    "go": ("https://github.com/tree-sitter/tree-sitter-go.git", "tree-sitter-go", "v0.13.3"),
-    "java": ("https://github.com/tree-sitter/tree-sitter-java.git", "tree-sitter-java", "v0.13.0"),
-    "javascript": ("https://github.com/tree-sitter/tree-sitter-javascript.git", "tree-sitter-javascript", "v0.13.0"),
-    "python": ("https://github.com/tree-sitter/tree-sitter-python.git", "tree-sitter-python", "v0.14.0"),
-    "php": ("https://github.com/tree-sitter/tree-sitter-php.git", "tree-sitter-php", "v0.13.0"),
-    "ruby": ("https://github.com/tree-sitter/tree-sitter-ruby", "tree-sitter-ruby", "v0.13.0")
+    # "go": ("https://github.com/tree-sitter/tree-sitter-go.git", "tree-sitter-go", "v0.13.3"),
+    # "java": ("https://github.com/tree-sitter/tree-sitter-java.git", "tree-sitter-java", "v0.13.0"),
+    # "javascript": ("https://github.com/tree-sitter/tree-sitter-javascript.git", "tree-sitter-javascript", "v0.13.0"),
+    "python": ("https://github.com/tree-sitter/tree-sitter-python.git", "tree-sitter-python", "v0.20.4"),
+    # "php": ("https://github.com/tree-sitter/tree-sitter-php.git", "tree-sitter-php", "v0.13.0"),
+    # "ruby": ("https://github.com/tree-sitter/tree-sitter-ruby", "tree-sitter-ruby", "v0.13.0")
 }
 
 def main():
@@ -22,7 +22,7 @@ def main():
         g = Git(str(repo_dir))
         g.checkout(tag)
         languages.append(str(repo_dir))
-    
+
     Language.build_library(
         # Store the library in the directory
         str(Path(function_parser.__path__[0])/"tree-sitter-languages.so"),
