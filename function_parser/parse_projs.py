0aa24b101e062326498fea64437ec1610e00f517
claudiosv <claudiosv@users.noreply.github.com>
2023-10-05 10:18:08 -0700

    Clean up

--- function_parser/parse_projs.py
+++ function_parser/parse_projs.py
@@ -7,35 +7,25 @@ from function_parser.language_data import LANGUAGE_METADATA
 from function_parser.process import DataProcessor
 from tree_sitter import Language
 from tqdm import tqdm
+from pathlib import Path
 
 language = "python"
 DataProcessor.PARSER.set_language(
-    Language(os.path.join(function_parser.__path__[0], "tree-sitter-languages.so"), language)
+    Language(
+        os.path.join(function_parser.__path__[0], "tree-sitter-languages.so"), language
+    )
 )
 processor = DataProcessor(
     language=language, language_parser=LANGUAGE_METADATA[language]["language_parser"]
 )
 
-for i in tqdm(range(1,51)):
-    dependee = f"project{i}"
+for i in tqdm(range(1, 51)):
+    dependee = (Path(os.environ['DYPYBENCH_PROJECTS_ROOT']) / f"project{i}").resolve()
     print(dependee)
-    definitions = processor.process_project(dependee, ext=LANGUAGE_METADATA[language]["ext"])
+    definitions = processor.process_project(
+        str(dependee), ext=LANGUAGE_METADATA[language]["ext"]
+    )
     df = pd.DataFrame(definitions)
-    df.to_parquet(f"df_project{i}.parquet")
+    df.to_parquet(f"dypybench_functions/df_project{i}.parquet")
+
 print("Done!")
-# conc = pd.concat(dfs)
-# conc.to_json("df_all.json")
-
-"""
-pip install coverage
-
-if [[ $1 == "scikit-learn" ]]
-
-then
-
-    timeout -k 10s $5 coverage run -m  pytest --import-mode=importlib $3 #tests for scikit-learn need importlib to locate conftest
-
-else
-
-    timeout -k 10s $5 coverage run -m  pytest $3
-    """
